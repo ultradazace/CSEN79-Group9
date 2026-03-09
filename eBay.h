b3c4d4c134cccf2528295816bf65fbcf5f965acf
jmontes14069 <Jose.montes98058@gmail.com>
2026-03-08 19:55:56 -0700

    Updated eBay.h
    I added new comments make sure to check them. I think these private members should work. All that is left are the public members and we can do the cpps.
    
    If you started working on any of the classes, make sure to make any changes wherever to make everything function if needed!

--- eBay.h
+++ eBay.h
@@ -1,27 +1,25 @@
 /*
- * This is my current progress on the .h file, just making the private members first then doing public later
- * User class is what 3A is on the file on canvas
- * Items class is what 3B is with Market class also being a part of that
+ * Updated and working on public member functions 
+ * This should cover the basics needed 
+ * 
  * 
  * Feel free to make changes or add anything new
  * Also leave ideas for public member functions
  */
 
-#ifndef EBAY.H
-#define EBAY.H
+#ifndef EBAY_H
+#define EBAY_H
 #include <string>
 #include <vector>
+#include <unordered_map>
+#include <queue>
 
 namespace ebay{
-    class User{
+    class Bid{
         public:
-        // Some getters and setters 
-        private:
-            std::string username;
-            bool isSeller;              // If true, then we initiate selling
-            Items* history[30];         // List has about 30 past entries (could make a new class for this)
-            Items selling;              // Only one item can be sold for now
-            Items* interest[];          // Items that the user wants to keep track of (could be a better way for this)
+            std::string user;
+            double amount;
+            bool operator <(const Bid& other) const {return amount < other.amount;}
     };
 
     class Items{
@@ -29,20 +27,33 @@ namespace ebay{
         // Same here from line 9
         private:
             std::string item_name;
-            bool win;                   // If this item was won in a bid
-            double sPrice;              // Starting price
-            double binPrice;            // "Buy it now price", if the user wants to skip the whole auction process and get it now
-            int expiration;             // When the item goes off sale in hours (There might be another way to do this)
+            bool win;                           // If this item was won in a bid
+            double sPrice;                      // Starting price
+            double binPrice;                    // "Buy it now price", if the user wants to skip the whole auction process and get it now
+            int expiration;                     // When the item goes off sale in hours (There might be another way to do this)
+            std::priority_queue<Bid> bids;      // The current bids for the item
     };
 
+    class User{
+        public:
+        // Some getters and setters 
+        private:
+            std::string username;
+            bool isSeller;                         // If true, then we initiate selling
+            Items* history[30];                    // List has about 30 past entries. I don't think we need to care about having all past entries
+            Items* selling;                        // Only one item can be sold for now
+            std::vector<Items*> interest;          // Items that the user wants to keep track of
+    };
+
+    // This is basically the main class
     class Market{
         public:
 
         private:
-            std::vector<Items> active_listings;
+            std::vector<Items> active_listings;             // Contains all items currently being auctioned
+            std::unordered_map<std::string, User> users;    // This is for our collection of users 
+            std::vector<std::string> logs;                  // The logs of previous transactions
     };
-
-
 }
 
 #endif
