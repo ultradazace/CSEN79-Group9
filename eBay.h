701a8bcfc39b7a51c733bc3f35ff55c549d4677d
jmontes14069 <Jose.montes98058@gmail.com>
2026-03-08 20:53:22 -0700

    Updated eBay.h
    This includes the public member functions for the classes. We may or may not need to change things here if things don't work.

--- eBay.h
+++ eBay.h
@@ -1,7 +1,7 @@
 /*
- * Updated and working on public member functions 
+ * Updated with public member functions
  * This should cover the basics needed 
- * 
+ * We might need to change things if needed
  * 
  * Feel free to make changes or add anything new
  * Also leave ideas for public member functions
@@ -17,14 +17,25 @@
 namespace ebay{
     class Bid{
         public:
+            Bid(const std::string& u, double a) : user(u), amount(a) {};
             std::string user;
             double amount;
-            bool operator <(const Bid& other) const {return amount < other.amount;}
+
+            bool operator <(const Bid& other) const {return amount < other.amount;}     // This is to check if bid is less than the bid we are checking
     };
 
     class Items{
         public:
-        // Same here from line 9
+            Items() : item_name(""), win(false), sPrice(0.0), binPrice(0.0), expiration(0.0) {};
+            void setItem(const std::string& name) {item_name = name;}
+            void setPrices(double& s, double& b);
+            void setTime(int& t) {expiration = t;}
+            std::string getItem() const {return item_name;}
+            double getPrice_S() const {return sPrice;}
+            double getPrice_B() const {return binPrice;}
+            int getTime() const {return expiration;}
+
+            void addBid(const Bid& b);          // This is to add and check if b is greater than the highest bid
         private:
             std::string item_name;
             bool win;                           // If this item was won in a bid
@@ -36,7 +47,14 @@ namespace ebay{
 
     class User{
         public:
-        // Some getters and setters 
+            User();
+            ~User();
+            void setUsername(const std::string& u);
+            void addHistory(const Items& add);
+            void startSelling() {isSeller = true;}
+
+            std::string getUsername() const {return username;}
+
         private:
             std::string username;
             bool isSeller;                         // If true, then we initiate selling
@@ -48,6 +66,11 @@ namespace ebay{
     // This is basically the main class
     class Market{
         public:
+            // Might need a constructor here but I don't know
+            ~Market();
+            void addItem(const Items& i);
+            void addUser(const User& u);
+            void addLog(const std::string& l);
 
         private:
             std::vector<Items> active_listings;             // Contains all items currently being auctioned
